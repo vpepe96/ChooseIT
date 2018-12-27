4e393b58b3662c10bff98cccb551156c4c4cc2a2
andreafasolino <44500241+andreafasolino@users.noreply.github.com>
2018-12-27 17:37:50 +0100

    Caricato ReportDAO

---
+++ ChooseIT/src/it/chooseit/dao/ReportDAO.java
@@ -0,0 +1,8 @@
+package it.chooseit.dao;
+
+import it.chooseit.bean.ReportBean;
+import it.chooseit.services.ReportKey;
+
+public interface ReportDAO extends GenericDAO<ReportBean, ReportKey> {
+
+}
