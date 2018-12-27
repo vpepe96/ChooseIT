fc2359d353e33229b9dc46c8cc5d531931169fe7
MariPS <marika.s.pia@gmail.com>
2018-12-27 16:37:54 +0100

    Modifica al GenericDAO.java

--- ChooseIT/src/it/chooseit/dao/GenericDAO.java
+++ ChooseIT/src/it/chooseit/dao/GenericDAO.java
@@ -10,35 +10,35 @@ public interface GenericDAO<T> {
 	
 	/**
 	 * Restituisce un oggetto di tipo T in base al codice.
-	 * @param code codice dell'elemento che si vuole ottenere
+	 * @param key chiave dell'elemento che si vuole ottenere
 	 * @return T oggetto contenente i dati dell'elemento desiderato
 	 * */
-	public T doRetrieveByKey(int code) throws SQLException;
+	public T retrieveByKey(Object key) throws SQLException;
 	
 	/**
 	 * Restituisce tutti gli elementi di tipo T nel DB.
 	 * @param order parametro in base al quale ordinare la collezione restituita
 	 * @return Collection<T> collezione contenente tutti gli oggetti del tipo desiderato
 	 * */
-	public Collection<T> doRetrieveAll(String order) throws SQLException; 
+	public Collection<T> retrieveAll(String order) throws SQLException; 
 	
 	/**
 	 * Inserisce un oggetto di tipo T.
 	 * @param object elemento che si desidera salvare nel DB
 	 * */
-	public void doSave(T object) throws SQLException;
+	public void insert(T object) throws SQLException;
 	
 	/**
 	 * Aggiorna un oggetto di tipo T.
 	 * @param object elemento che si desidera aggiornare nel DB
 	 * */
-	public void doUpdate(T object) throws SQLException;
+	public void update(T object) throws SQLException;
 	
 	/**
 	 * Elimina un oggetto di tipo T in base al codice.
-	 * @param code codice dell'elemento che si vuole eliminare
+	 * @param key codice dell'elemento che si vuole eliminare
 	 * @return boolean true se l'oggetto è stato eliminato, false altrimenti
 	 * */
-	public boolean doDelete(int code) throws SQLException;
+	public boolean delete(Object key) throws SQLException;
 	
 }
