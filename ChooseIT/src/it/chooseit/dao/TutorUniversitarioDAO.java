f318497f0de525a3b6f23b41a695101bc70f0691
Mario <mario.siglioccolo999@gmail.com>
2018-12-27 19:24:19 +0100

    Caricata la classe TutorUniversitarioDAO

---
+++ ChooseIT/src/it/chooseit/dao/TutorUniversitarioDAO.java
@@ -0,0 +1,12 @@
+package it.chooseit.dao;
+
+import it.chooseit.bean.TutorUniversitarioBean;
+
+/**
+ * Interfaccia che estende GenericDAO e definisce ulteriori operazioni individuali del Tutor Universitario.
+ *  
+ * @author RocketStudios
+ */
+public interface TutorUniversitarioDAO extends GenericDAO<TutorUniversitarioBean, String>{
+
+}
