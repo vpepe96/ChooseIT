2c041afc3f00fcea509f46f6215f0a7886bd4ff9
andreafasolino <44500241+andreafasolino@users.noreply.github.com>
2018-12-27 17:21:11 +0100

    Aggiunta classe ReportKey nel package it.chooseit.services

---
+++ ChooseIT/src/it/chooseit/services/ReportKey.java
@@ -0,0 +1,35 @@
+package it.chooseit.services;
+
+import java.sql.Date;
+
+import it.chooseit.bean.RegistroTirocinioBean;
+
+public class ReportKey {
+	private RegistroTirocinioBean registro;
+	private Date data;
+	
+	
+	
+	public ReportKey(RegistroTirocinioBean registro, Date data) {
+		super();
+		this.registro = registro;
+		this.data = data;
+	}
+	
+	
+	public RegistroTirocinioBean getRegistro() {
+		return registro;
+	}
+	public void setRegistro(RegistroTirocinioBean registro) {
+		this.registro = registro;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	
+	
+
+}
