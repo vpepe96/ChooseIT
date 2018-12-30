16d9aacb8c66daba514d348d381600be1aa651d0
andreafasolino <44500241+andreafasolino@users.noreply.github.com>
2018-12-30 16:58:28 +0100

    Caricata GestioneAreaPersonaleFacade

---
+++ ChooseIT/src/it/chooseit/facade/GestioneAreaPersonaleFacade.java
@@ -0,0 +1,45 @@
+package it.chooseit.facade;
+
+import java.sql.SQLException;
+
+import it.chooseit.bean.StudenteBean;
+import it.chooseit.bean.UtenteBean;
+import it.chooseit.impl.Studente;
+import it.chooseit.impl.Utente;
+
+/**
+ * Definisce i metodi per gestire le funzionalità relative all’area personale.
+ */
+public class GestioneAreaPersonaleFacade {
+	
+	/**
+	 * Permette ad un utente generico di modificare il proprio profilo.
+	 * @param utente utente da modificare
+	 * @return boolean true se la modifica è avvenuta, false altrimenti 
+	 * */
+	public boolean modificaProfilo(UtenteBean utente) {
+		Utente ut=new Utente();
+		try {
+			ut.update(utente);
+			return true;
+		} catch (SQLException e) {
+			return false;
+		}	
+	}
+	
+	/**
+	 * Permette ad uno studente di modificare il proprio profilo.
+	 * @param studente studente da modificare
+	 * @return boolean true se la modifica è avvenuta, false altrimenti 
+	 * */
+	public boolean modificaProfilo(StudenteBean studente) {
+		Studente stud=new Studente();
+		try {
+			stud.update(studente);
+			return true;
+		} catch (SQLException e) {
+			return false;
+		}	
+	}
+
+}
