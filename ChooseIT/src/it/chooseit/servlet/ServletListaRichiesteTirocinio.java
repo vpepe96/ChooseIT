f2bb0327af401b00f4e9fbfc51690497cd24a29b
andreafasolino <44500241+andreafasolino@users.noreply.github.com>
2018-12-31 15:15:22 +0100

    Prova correzione ServletRichiesteTirocinio

--- ChooseIT/src/it/chooseit/servlet/ServletListaRichiesteTirocinio.java
+++ ChooseIT/src/it/chooseit/servlet/ServletListaRichiesteTirocinio.java
@@ -17,7 +17,7 @@ import it.chooseit.dao.RichiestaTirocinioDAO;
 import it.chooseit.facade.GestionePraticheTirocinioFacade;
 import it.chooseit.impl.RichiestaTirocinio;
 
-@WebServlet("ServletListaRichiesteTirocinio")
+@WebServlet("/ServletListaRichiesteTirocinio")
 public class ServletListaRichiesteTirocinio extends HttpServlet{
 	
 	private static final long serialVersionUID = 1L;
